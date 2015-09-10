7bb691d05949fd8d0714e0a00054030109ab40ad
Perilon <aewilkin@andrew.cmu.edu>
2015-09-10 18:32:55 -0400

    First commit

---
+++ ch1/Solution01.java
@@ -0,0 +1,38 @@
+/**
+ * Created by perilon on 9/10/15.
+ */
+
+import java.io.*;
+
+public class Solution01 {
+
+    public static void main(String[] args) throws IOException {
+        String inputString;
+        System.out.print("Enter a string: ");
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        inputString = input.readLine();
+
+        boolean allUnique = true;
+        int i = 0;
+        String temp = "";
+
+//        For each character in the string, add the character to string "temp" and iterate through (all but
+//        the newest character of) temp to see if the character already exists in it
+
+        while (i < inputString.length() && allUnique == true) {
+
+            char c = inputString.charAt(i);
+            temp += c;
+
+            for (int n = 0; n < temp.length()-1; n++) {
+
+                if (temp.charAt(n) == c) {
+
+                    allUnique = false;
+                }
+            }
+            i++;
+        }
+        System.out.println("It is " + Boolean.toString(allUnique) + " that the string has all unique characters.");
+    }
+}
---
+++ ch1/Solution02.java
@@ -0,0 +1,55 @@
+/**
+ * Created by perilon on 9/10/15.
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class Solution02 {
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.print("Enter a string: ");
+        BufferedReader input1 = new BufferedReader(new InputStreamReader(System.in));
+        String str1 = input1.readLine();
+        System.out.print("Enter another string: ");
+        BufferedReader input2 = new BufferedReader(new InputStreamReader(System.in));
+        String str2 = input2.readLine();
+
+        System.out.println("It is " + Boolean.toString(isAPermutation(str1, str2)) + " that " +
+                "each string is a permutation of the other.");
+    }
+
+    public static boolean isAPermutation(String s1, String s2) {
+
+        Map<Character, Integer> map1 = new TreeMap<Character, Integer>();
+        Map<Character, Integer> map2 = new TreeMap<Character, Integer>();
+
+        for (int i = 0; i < s1.length(); i++) {
+            char c = s1.charAt(i);
+            if (map1.get(c) == null) {
+                map1.put(c, 1);
+            }
+            else {
+                int curVal = map1.get(c);
+                map1.put(c, curVal + 1);
+            }
+        }
+
+        for (int i = 0; i < s2.length(); i++) {
+            char c = s2.charAt(i);
+            if (map2.get(c) == null) {
+                map2.put(c, 1);
+            }
+            else {
+                int curVal = map2.get(c);
+                map2.put(c, curVal + 1);
+            }
+        }
+
+        boolean answer = map1.equals(map2);
+
+        return answer;
+    }
+
+}
